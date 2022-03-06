4655f1f89ae31b453a4fc9c98e0553d541d3956a
kwYoohae <gocksdl7979@gmail.com>
2022-03-06 16:12:20 +0900

    [추가] 초기설정

---
+++ src/main/java/com/kwyoohae/kstartupslackbot/KStartUpSlackBotApplication.java
@@ -0,0 +1,13 @@
+package com.kwyoohae.kstartupslackbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KStartUpSlackBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KStartUpSlackBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwyoohae/kstartupslackbot/KStartUpSlackBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwyoohae.kstartupslackbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KStartUpSlackBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
